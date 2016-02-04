066f6ae068d2c46c1e9e765d47ab275e5af1b151
Austin Poulton <austin@percepts.eu>
2016-02-04 21:01:58 +0000

    First commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "photo-search"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+resolvers ++= Seq(
+  "Scala libs" at "http://scala-tools.org/repo-releases",
+  "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
+)
+
+libraryDependencies ++= Dependencies.projectDeps
---
+++ project/Dependencies.scala
@@ -0,0 +1,38 @@
+import sbt._
+
+object Version {
+  val mockito    = "1.10.19"
+  val scala      = "2.10.5"
+  val scalaTest  = "2.2.4"
+  val lucence    = "4.0.0"
+  val es         = "2.1.1"
+  val nlp        = "3.6.0"
+  val flickr     = "2.15"
+  //val sparkTest  = "1.3.0_0.2.0"
+}
+
+object Library {
+
+
+  val mockitoAll      = "org.mockito"       %  "mockito-all"     % Version.mockito
+  val scalaTest       = "org.scalatest"     %% "scalatest"       % Version.scalaTest
+  val luceneCore      = "org.apache.lucene"  % "lucene-core"  % Version.lucence
+  val luceneAnalyzer  = "org.apache.lucene"  % "lucene-analyzers-common"  % Version.lucence
+  val luceneQuery     = "org.apache.lucene"  % "lucene-queryparser"  % Version.lucence
+  val esCore          = "org.elasticsearch" % "elasticsearch" %  Version.es
+  val nlpCore         = "edu.stanford.nlp" % "stanford-corenlp" % Version.nlp
+  val flickr4j        = "com.flickr4java" % "flickr4java" % Version.flickr
+}
+
+object Dependencies {
+
+  import Library._
+
+  val projectDeps = Seq(
+    luceneCore,
+    esCore,
+    nlpCore,
+    scalaTest      % "test",
+    mockitoAll     % "test"
+  )
+}
