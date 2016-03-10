19e1fb035eb0d45db4bf87a34a165855530d8284
Austin Poulton <austin@percepts.eu>
2016-03-10 18:58:15 +0000

    [AP] Test for CaptionLoader
    tested loading all 8092 image documents.

--- src/main/scala/co/semanticiris/service/CaptionLoader.scala
+++ src/main/scala/co/semanticiris/service/CaptionLoader.scala
@@ -17,11 +17,11 @@ class CaptionLoader(val captionFileName: String = "/Flickr8k.token.txt") {
     val captionStrs = Source.fromURL(getClass.getResource(captionFileName)).getLines().toList
     // parse each string into a caption: Caption(s)
     // then map each caption into a tuple (imageId, captionId, caption): map(c => (c.photoId, c.captionId, c))
-    val captionTupleList = captionStrs.map(s => Caption(s)).filter(copt => copt == Some).map(c => (c.get.photoId, c.get.captionId, c.get))
+    val captionTupleList = captionStrs.map(s => Caption(s)).filter(copt => copt != None).map(c => (c.get.photoId, c.get.captionId, c.get))
 
     // now things get interesting, map each tuple into a HashMap[PhotoId, ImageDoc]
     val photoDocMap = captionTupleList.foldLeft(new HashMap[String, ImageDocument]) { (imgDocMap, capTuple) =>
-      if (imgDocMap(capTuple._1) == None) imgDocMap + (capTuple._1 -> ImageDocument(capTuple._1, capTuple._3))
+      if (imgDocMap.get(capTuple._1) == None) imgDocMap + (capTuple._1 -> ImageDocument(capTuple._1, capTuple._3))
       else imgDocMap + (capTuple._1 -> (imgDocMap(capTuple._1) + capTuple._3))
     }
     photoDocMap
--- src/test/scala/co/semanticiris/service/CaptionLoaderSpec.scala
+++ src/test/scala/co/semanticiris/service/CaptionLoaderSpec.scala
@@ -1,5 +1,6 @@
 package co.semanticiris.service
 
+import co.semanticiris.model.Caption
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 
 import scala.io.Source
@@ -7,18 +8,16 @@ import scala.io.Source
 /**
   * Created by austin on 08/03/2016.
   */
-class CaptionLoaderSpec extends FlatSpec with BeforeAndAfter {
+class CaptionLoaderSpec extends FlatSpec  {
 
   lazy val flickrCaptionStrs = Source.fromURL(getClass.getResource("/Flickr8k.token.txt")).getLines().toList
   lazy val testCaptionStrs = Source.fromURL(getClass.getResource("/Test.Captions.txt")).getLines().toList
 
-  before {
-    println("Read captions from file "+ testCaptionStrs.length)
+  "A CaptionLoader" should "load all captions into a image document map" in {
+    val captionLoader = new CaptionLoader()
+    val imgDocMap = captionLoader.loadCaptions()
+    println("image document map: "+imgDocMap.size)
+    // assert we have got the full 8k img documents in the map
+    assert(imgDocMap.size == 8092)
   }
-
-  after {
-    println("****")
-  }
-
-
 }
